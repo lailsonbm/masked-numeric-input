0bbfd2be147317f5469b6c186b4627b1628d97f3
Lailson Bandeira <lailson@guava.com.br>
2018-10-24 19:16:45 -0300

    Sets exported UMD module name

--- bili.config.js
+++ bili.config.js
@@ -3,6 +3,7 @@ const vue = require('rollup-plugin-vue')
 module.exports = {
   banner: true,
   format: ['umd', 'umd-min'],
+  moduleName: 'MaskedNumericInput',
   css: true,
   plugins: [
     vue({ css: true })
