a0e0d6e9772c6d76469ea869dc2d3cfb5c542ebd
Lailson Bandeira <lailson@guava.com.br>
2018-10-24 09:39:01 -0300

    Bootstraps project
    Generated with the command `vue init biigpongsatorn/vue-lib-template masked-numeric-input`.

---
+++ src/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello World 🌍 </h1><br>
+    <span> Welcome to masked-numeric-input 👏🏻 </span><br>
+    <span>📝 Numeric mask for text inputs</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'masked-numeric-input'
+}
+</script>
+
+<style scoped>
+</style>
